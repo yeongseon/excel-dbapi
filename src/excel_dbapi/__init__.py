6bdfd7250d57d3f8ead1b04e57108128d76bec46
Yeongseon Choe <yeongseon.choe@gmail.com>
2025-04-03 04:59:03 +0000

    feat: expose all public APIs in __init__.py

--- src/excel_dbapi/__init__.py
+++ src/excel_dbapi/__init__.py
@@ -1,1 +1,14 @@
-__version__ = "0.1.0"
+from .api import connect
+from .connection import Connection, Cursor
+from .executor import execute_query
+from .parser import SQLParser
+from .exceptions import ExcelDBAPIError
+
+__all__ = [
+    "connect",
+    "Connection",
+    "Cursor",
+    "execute_query",
+    "SQLParser",
+    "ExcelDBAPIError",
+]
